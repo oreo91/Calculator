9ee5b63d730c350edb7002c33f69abbb93e984da
oreo91 <73924170+oreo91@users.noreply.github.com>
2022-05-24 19:45:53 -0400

    Add files via upload

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Calculator {
+    // main method
+    public static void main(String[] args) {
+        Calculator myCalculator = new Calculator();
+        Scanner input = new Scanner(System.in);
+        
+
+
+
+
+
+    }
+
+   // constructor method
+   public Calculator() {
+
+   }
+
+   // add method
+    public int add(int a, int b) {
+
+        return a + b;
+    }
+
+    // subtract method
+    public int subtract(int a, int b){
+
+        return a - b;
+    }
+
+    // multiply method
+    public int multiply(int a, int b) {
+
+        return a * b;
+    }
+
+    // divide method
+    public int divide(int a, int b) {
+
+        return a / b;
+    }
+
+    // modulo method
+    public int modulo(int a, int b) {
+
+        return a % b;
+    }
+}
